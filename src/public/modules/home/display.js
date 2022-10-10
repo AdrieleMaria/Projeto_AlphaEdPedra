2858bdb625926417f63f9240e93fa342146a8d6e
Adriele Maria <adrieleifrn@gmail.com>
2022-10-10 20:43:23 -0300

    add: display

--- src/public/modules/home/display.js
+++ src/public/modules/home/display.js
@@ -1,60 +1,59 @@
-// import { displayRender } from "./pages/inventoriesRender.js";
+import { displayRender } from "./pages/displayRender.js";
 
 async function displayReq() {
 
-    fetch(`http://localhost:8082/listpedra`, {
-        method: "GET",
-        // body: JSON.stringify(_user),
-        //enviar o token para autorização
-        headers: { "Content-type": "application/json; charset=UTF-8" },
-    })
-        .then((res) => res.json())
-        .then((dados) => {
-            console.log(dados);
-            // if (dados === "Login sucedido") {
-            //     body.innerHTML = homePage();
-            // }
-        })
-        .catch((erro) => {
-            console.log(erro);
-            document.getElementById("status").innerHTML = `${erro}`;
+    const token = localStorage.getItem("auth");
+
+    try {
+        const response = await fetch(`http://localhost:8082/listpedra`, {
+            method: "GET",
+            headers: { "Content-type": "application/json; charset=UTF-8", "Authorization": `Basic ${token}` },
         });
+
+        const data = await response.json();
+
+        if (!response.ok) throw new Error(data.err);
+
+        console.log(data);
+        //retornando o objeto >>>>>>>> OK!!!!!
+        const display = document.getElementById("display_box");
+        display.innerHTML = ``;
+
+        data.data.forEach(element => {
+            // listIcon
+
+
+
+            if(element.validated === true){
+                display.innerHTML += `
+                <button class="stone">
+                    <img width="100%" height="100%" src="${element.img_url}" />
+                </button>             
+                `;
+            }
+
+        });
+
+
+    } catch (error) {
+
+        console.log(error);
+
+    }
 
 }
 
 function display() {
-
-
+    document.getElementById("appHome").innerHTML = ``;
+    document.getElementById("appHome").innerHTML = displayRender();
+    displayReq();
     console.log("display");
 
-
-
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
 window.display = display;
 export { display }
+
+
---
+++ src/public/modules/home/pages/displayRender.js
@@ -0,0 +1,12 @@
+export function displayRender(_id) {
+    return `
+    <div class="display_center">
+        <div id="display_box">
+        </div>
+
+        <div id="pages_div">
+        </div>
+    </div>
+   
+    `
+}
